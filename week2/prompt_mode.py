81e80ce897f34f2561eefc9c06fd1266d53b7a43
Izzat Redza <izzatredza4@gmail.com>
2026-06-15 09:51:59 +0800

    chore: delete main.py in favor or prompt_mode.py

--- week2/prompt_mode.py
+++ week2/prompt_mode.py
@@ -1,3 +1,7 @@
 def prompt_model(model: str, prompt: str) -> str:
     # Your code here...
     pass
+
+
+if __name__ == "__prompt_mode__":
+    prompt_model("gpt-3.5-turbo", "What is the capital of France?")
